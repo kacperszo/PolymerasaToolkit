33cb085e176a350dd2652d062f951b83ed4a463c
kacperszo <kacperszot1@gmail.com>
2024-05-09 11:59:19 +0200

    bump scala to 3.4.1

--- build.sbt
+++ build.sbt
@@ -1,5 +1,3 @@
 name := "PolymerasaToolkit"
 version := "0.1.0-SNAPSHOT"
-scalaVersion := "2.13.13"
-
-libraryDependencies += "org.apache.spark" %% "spark-sql" % "3.5.1"
+scalaVersion := "3.4.1"
--- src/main/scala/polimerasa/AminoAcids.scala
+++ src/main/scala/polimerasa/AminoAcids.scala
@@ -14,13 +14,11 @@ object AminoAcids{
   final case object LEUCINE extends AminoAcids("L", "Leu", Array("UUA", "UUG", "CUU", "CUC", "CUA", "CUG"))
   final case object METHIONINE extends AminoAcids("M", "Met", Array("AUG"))
   final case object ASPARAGINE extends AminoAcids("N", "Asn", Array("AAU", "AAC"))
-//  final case object PYRROLYSINE extends AminoAcids("O", "Pyl", Array()) doesnt exist in humans so it's out of scope of the PolymerasaToolkit
   final case object PROLINE extends AminoAcids("P", "Pro", Array("CCU", "CCC", "CCA", "CCG"))
   final case object GLUTAMINE extends AminoAcids("Q", "Gln", Array("CAA", "CAG"	))
   final case object ARGININE extends AminoAcids("R", "Arg", Array("CGU", "CGC", "CGA", "CGG", "AGA", "AGG"))
   final case object SERINE extends AminoAcids("S", "Ser", Array("UCU", "UCC", "UCA", "UCG", "AGU", "AGC"))
   final case object THREONINE extends AminoAcids("T", "Thr", Array("ACU", "ACC", "ACA", "ACG"))
-//  final case object SELENOCYSTEINE extends AminoAcids("U", "Sec", Array()) doesnt exist in humans so it's out of scope of the PolymerasaToolkit
   final case object VALINE extends AminoAcids("V", "Val", Array("GUU", "GUC", "GUA", "GUG"))
   final case object TRYPTOPHAN extends AminoAcids("W", "Trp", Array("UGG"))
   final case object TYROSINE extends AminoAcids("Y", "Tyr", Array("UAU, UAC"))
