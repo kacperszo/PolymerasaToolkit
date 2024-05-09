305b261dbdbe0364f1545df2d9861100232d9f6f
JBRS307 <kuba.rekas.307@gmail.com>
2024-05-09 12:57:29 +0200

    DNA complement and reverse

--- src/main/scala/polimerasa/DNA.scala
+++ src/main/scala/polimerasa/DNA.scala
@@ -4,4 +4,20 @@ class DNA(sequence: Array[NitrogenousBases]) {
   def getSequence(): Array[NitrogenousBases] = {
     sequence
   }
+
+  def complement(): Array[NitrogenousBases] = {
+    var res = Array[NitrogenousBases]()
+    for base <- sequence do {
+      res :+= base
+    }
+    res
+  }
+
+  def reverse_complement(): Array[NitrogenousBases] = {
+    this.complement().reverse
+  }
+
+  def reverse(): Array[NitrogenousBases] = {
+    this.sequence.reverse
+  }
 }
