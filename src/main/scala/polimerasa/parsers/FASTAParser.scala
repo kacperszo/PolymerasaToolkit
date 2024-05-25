272fea21b5298471524b16b238075c327576e095
Kacper Szot <kacperszot1@gmail.com>
2024-05-25 21:54:06 +0200

    Make FASTAParser functionally pure

--- src/main/scala/polimerasa/parsers/FASTAParser.scala
+++ src/main/scala/polimerasa/parsers/FASTAParser.scala
@@ -2,9 +2,14 @@ package polimerasa.parsers
 
 import polimerasa.{DNA, NitrogenousBases, RNA}
 
+import scala.+:
+import scala.annotation.tailrec
 import scala.collection.mutable.ArrayBuffer
+import scala.util.Using
+import scala.io.Source
+import scala.language.postfixOps
 
-case class FASTASequence(name: String, sequence: ArrayBuffer[NitrogenousBases]){
+case class FASTASequence(name: String, sequence: Seq[NitrogenousBases]){
   def toDNA: DNA = {
     DNA(sequence.toArray)
   }
@@ -12,27 +17,34 @@ case class FASTASequence(name: String, sequence: ArrayBuffer[NitrogenousBases]){
   def toRNA: RNA = {
     RNA(sequence.toArray)
   }
-  def toProtein: Unit = {
+  def toProtein(): Unit = {
     throw new NotImplementedError()
   }
 }
 
 object FASTAParser {
 
-  def parseFile(filePath: String): Array[FASTASequence] = {
-    val bufferedSource = scala.io.Source.fromFile(filePath)
-    val sequences = ArrayBuffer[FASTASequence]()
-    var currentSequence: Int = -1
-    for (lines <- bufferedSource.getLines()) {
-      if (lines.contains(">")) {
-        currentSequence += 1
-        sequences.addOne(FASTASequence(lines.substring(1),new ArrayBuffer[NitrogenousBases]))
-      } else {
-        lines.toUpperCase().foreach(base => {
-          sequences(currentSequence).sequence.addOne(NitrogenousBases.fromLetter(base))
-        })
-      }
+  def parseFile(filePath: String): Seq[FASTASequence] = {
+
+    @tailrec
+    def parseLine(line: String, acc: Seq[NitrogenousBases]): Seq[NitrogenousBases] = {
+      if(line.isEmpty) acc
+      else parseLine(line.substring(1), acc:+NitrogenousBases.fromLetter(line.toUpperCase.charAt(0)))
     }
-    sequences.toArray
+
+    @tailrec
+    def parseLines(lines: Iterable[String], currentSeq: Option[FASTASequence], sequences: Seq[FASTASequence]): Seq[FASTASequence] = {
+
+      lines match{
+        case Nil => if (currentSeq.nonEmpty) sequences :+ currentSeq.get else sequences
+        case line :: xs if line.startsWith(">") => parseLines(xs, Some[FASTASequence]( FASTASequence(line.substring(1), Seq.empty[NitrogenousBases])), if (currentSeq.nonEmpty) sequences :+ currentSeq.get else sequences)
+        case line :: xs => parseLines(xs, if(currentSeq.nonEmpty) Some[FASTASequence](FASTASequence(currentSeq.get.name, currentSeq.get.sequence ++ parseLine(line, Seq.empty[NitrogenousBases]))) else currentSeq, sequences)
+      }
+
+    }
+
+    Using(Source.fromFile(filePath)) { source =>
+      parseLines(source.getLines().toSeq, None, Seq.empty[FASTASequence])
+    }.getOrElse(throw new RuntimeException(s"Failed to read file: $filePath") )
   }
 }
--- src/test/scala/polimerasa/parsers/FASTAParserTest.scala
+++ src/test/scala/polimerasa/parsers/FASTAParserTest.scala
@@ -5,14 +5,12 @@ import org.scalatest.funsuite.AnyFunSuiteLike
 class FASTAParserTest extends AnyFunSuiteLike {
 
   test("test file parsing using sample 1") {
-    val result = FASTAParser.parseFile("sample1.fasta")
-    println(result(0))
-    println(result(1))
-    println(result(2))
+    val result = FASTAParser.parseFile("./sample1.fasta").toArray
+    assert(result.length==3)
   }
   test("test file parsing using sample 2") {
-    val result = FASTAParser.parseFile("sample2.fasta")
-    println(result(0))
+    val result = FASTAParser.parseFile("./sample2.fasta").toArray
+    assert(result.length==1)
   }
 
 }
